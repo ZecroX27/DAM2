ac15db64155e213ef88a553aba887ba4f148f4bf
ZecroX27 <yoel4352@gmail.com>
2025-10-24 12:48:49 +0200

    Creacion OperacionesCRUD

--- acceso a datos/SQL/SQL/src/main/java/EjerciciosJDBC/OperacionesCRUD.java
+++ acceso a datos/SQL/SQL/src/main/java/EjerciciosJDBC/OperacionesCRUD.java
@@ -1,4 +1,48 @@
 package EjerciciosJDBC;
 
+import java.sql.Statement;
+import java.sql.*;
+
 public class OperacionesCRUD {
+
+    public static void insertarEmpleado (){
+        try(Connection c = Conexion.getConnection();
+            Statement stmt = c.createStatement();){
+            String sql = "INSERT INTO empleados (nombre, apellido, salario, departamento_id) values ('Oscar', 'Carrillo', 1740.50, 3)";
+            stmt.executeUpdate(sql);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public static void listarEmpleados (){
+        try(Connection c = Conexion.getConnection();
+            Statement stmt = c.createStatement();){
+            String sql = "SELECT * FROM empleados";
+            stmt.executeQuery(sql);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public static void ActualizarSalario (){
+        try(Connection c = Conexion.getConnection();
+            Statement stmt = c.createStatement();){
+            String sql = "UPDATE empleados SET salario = 1230,45 WHERE empleados.nombre = 'Oscar'";
+            stmt.executeUpdate(sql);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public static void eliminarEmpleado (){
+        try(Connection c = Conexion.getConnection();
+            Statement stmt = c.createStatement();){
+            String sql = "DELETE FROM empleados WHERE empleados.nombre = 'Oscar'";
+            stmt.executeUpdate(sql);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
 }
