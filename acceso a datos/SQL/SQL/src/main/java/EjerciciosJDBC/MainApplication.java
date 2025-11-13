bbb3902158a912921a8b69e3a35da04047719b83
ZecroX27 <yoel4352@gmail.com>
2025-11-13 13:18:43 +0100

    Empezando XML

--- acceso a datos/SQL/SQL/src/main/java/EjerciciosJDBC/MainApplication.java
+++ acceso a datos/SQL/SQL/src/main/java/EjerciciosJDBC/MainApplication.java
@@ -4,19 +4,18 @@ import java.sql.SQLException;
 
 public class MainApplication  {
     public static void main(String[] args)throws SQLException {
-        Conexion conexion = new Conexion();
-        OperacionesCRUD operacionesCRUD = new OperacionesCRUD();
+
         //operacionesCRUD.listarEmpleados();
         //Result.navegarResultSetScrolleable();
         //Result.actualizarSalariosViaResultSet();
         //Result.insertarEmpleadosViaResultSet();
         //Result.eliminarEmpleadoViaResultSet(9);
-        Transacciones transacciones = new Transacciones();
+
         //transacciones.transferirSalario(1, 2, 500);
         //transacciones.actualizacionMasivaSalarios();
         //EmpleadosPreparedStatement.insertarEmpleadoPrepared("Oscar", "Galinsoga", 30000.00, 3);
         //EmpleadosPreparedStatement.buscarEmpleadosPorDepartamento(3);
-        EmpleadosPreparedStatement.actualizarSalarioPrepared(6, 25000);
-        //Result.navegarResultSetForward();
+        //EmpleadosPreparedStatement.actualizarSalarioPrepared(6, 25000);
+        Result.navegarResultSetForward();
     }
 }
